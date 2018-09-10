c14b21fb74c10def9629775f5bdcf6f910f0441a
Brian <qweasdzxc123090@gmail.com>
2018-09-10 22:56:23 +0800

    delete website

--- website/controller/framework/doc_footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-class ControllerFrameworkDocFooter extends Controller {
-	public function index() {
-		$data['scripts'] = $this->document->getScripts('footer');
-		$data['theme'] = $this->_theme;
-		return $this->load->view('framework/doc_footer', $data);
-	}
-}
--- website/language/zh-TW/extension/total/shipping.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Text
-$_['text_total_shipping'] = '運費小計';
-$_['text_total_shipping_free'] = '免運費';
